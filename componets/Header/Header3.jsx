d0f74c7d098df9e20381207c78075490d84cec3e
Deepfresh-star <deep.vithani@freshcodes.net>
2025-09-08 17:09:39 +0530

    feat : header six icon logo update

--- componets/Header/Header3.jsx
+++ componets/Header/Header3.jsx
@@ -3,6 +3,15 @@ import Link from "next/link";
 import React from "react";
 
 const Header3 = () => {
+  const accreditations = [
+    { img: "/assets/joint-comission.svg", alt: "Joint Commission International" },
+    { img: "/assets/AmericanHeartAssociation.svg", alt: "American Heart Association" },
+    { img: "/assets/AmericanHeartAssociation.svg", alt: "American Stroke Association" },
+    { img: "/assets/NABH.svg", alt: "NABH" },
+    { img: "/assets/rahiyaccordiongreen.svg", alt: "NABH" },
+    { img: "/assets/mcseventwo.svg", alt: "NABL" },
+  ];
+
   return (
     <div>
       <header
@@ -11,7 +20,7 @@ const Header3 = () => {
           background: "linear-gradient(84deg, #F2D5CF 0%, #E2EEFE 100%)",
         }}
       >
-        <div className="container flex justify-between">
+        <div className="container flex justify-between items-center">
           <div className="">
             <Link href="/">
               <Image
@@ -23,14 +32,17 @@ const Header3 = () => {
               />
             </Link>
           </div>
-          <div className="">
-            <Image
-              src="/assets/headermainlogo.svg"
-              alt="header-thaumbnail"
-              width={420}
-              height={60}
-            //   className="w-[555px]"
-            />
+          <div className="flex items-center gap-4">
+            {accreditations.map((item, index) => (
+              <Image
+                key={index}
+                src={item.img}
+                alt={item.alt}
+                width={64}
+                height={64}
+                className="w-16 h-16"
+              />
+            ))}
           </div>
           <div>
             <h2 className="min-[1200px]:text-[42px] text-[30px] font-bold Text-color2">#LifeGetsBetter</h2>
