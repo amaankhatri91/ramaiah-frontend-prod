86133a2c00e56ed0dea88826f8c97f5bd5dabefd
Deepfresh-star <deep.vithani@freshcodes.net>
2025-10-09 18:03:16 +0530

    feat : mega menu done

--- componets/Header/Header2.jsx
+++ componets/Header/Header2.jsx
@@ -121,7 +121,7 @@ const Header2 = () => {
           </Link>
 
           {isOpen && (
-            <div className="absolute cursor-pointer min-[1410px]:left-[380px] min-[1210px]:left-[300px] left-[300px] min-[1400px]:max-h-[600px] max-h-[500px] overflow-auto transform -translate-x-1/2 top-[68px] w-[90vw] min-[1410px]:max-w-[75vw] max-w-[90vw] bg-white rounded-lg shadow-2xl border border-gray-200 p-6 z-50">
+            <div className="absolute cursor-pointer min-[1410px]:left-[520px] min-[1310px]:left-[470px] left-[400px] min-[1400px]:max-h-[600px] max-h-[500px] overflow-auto transform -translate-x-1/2 top-[68px] w-[90vw] min-[1410px]:max-w-[75vw] max-w-[80vw] bg-white rounded-lg shadow-2xl border border-gray-200 p-6 z-50">
               {renderSpecialtiesMenu(item.children)}
             </div>
           )}
@@ -231,7 +231,7 @@ const Header2 = () => {
                   <div key={center.id} className="relative" onMouseEnter={() => { setOpenCenterKey(center.id); setOpenChildSlug(null); }}>
                     <Link
                       href={generateUrl(center)}
-                      className="cursor-pointer w-full h-full flex items-center justify-between px-3 py-[14px] text-left text-[#3D3D3D] rounded-[18px] bg-[linear-gradient(95deg,_#FBFDFF_0.79%,_#E9F6FF_98.08%)] hover:text-[#e14b8b] text-[12px] min-[1190px]:text-[18px] font-semibold"
+                      className="cursor-pointer w-full h-full flex items-center justify-between px-3 py-[14px] text-left text-[#3D3D3D] rounded-[18px] bg-[linear-gradient(95deg,_#FBFDFF_0.79%,_#E9F6FF_98.08%)] hover:text-[#e14b8b] text-[12px] min-[1190px]:text-[16px] "
                       onClick={() => setOpenMenuItems(prev => ({ ...prev, [section.id]: false }))}
                     >
                       <span className="flex gap-2 flex-1 text-left">
@@ -318,7 +318,7 @@ const Header2 = () => {
                         style={{
                           background: "var(--White-Ice-Line, linear-gradient(95deg, #FBFDFF 0.79%, #E9F6FF 98.08%))",
                         }}
-                        className="flex px-3 py-[14px] text-[#3D3D3D] rounded-[18px] bg-[linear-gradient(95deg,_#FBFDFF_0.79%,_#E9F6FF_98.08%)] hover:text-[#e14b8b] text-[12px] min-[1190px]:text-[18px] font-semibold transition-colors"
+                        className="flex px-3 py-[14px] text-[#3D3D3D] rounded-[18px] bg-[linear-gradient(95deg,_#FBFDFF_0.79%,_#E9F6FF_98.08%)] hover:text-[#e14b8b] text-[12px] min-[1190px]:text-[16px]  transition-colors"
                         onClick={() => setOpenMenuItems(prev => ({ ...prev, [section.id]: false }))}
                       >
                         <Image src="/assets/ramhaiyaison.svg" alt="icon" width={18} height={18} className="w-[18px] h-[18px] mr-[4px] min-[1200px]:mt-[3px]" />
